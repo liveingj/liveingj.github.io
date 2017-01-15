60408352fa9d2c3024ebeda28cf7047ecbe1f22d
liveingj <liveingj@gmail.com>
2017-01-15 17:23:49 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function askQuestions() {
+
+
+var firstName = prompt('what is your first name?'),
+	lastName = prompt('what is your last name?'),
+	fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+var age = prompt('how old are you?');
+age = parseInt(age);
+
+if (age >= 20) {
+
+	console.log('user is an adult');
+
+} else if (age >= 13) {
+
+		console.log('User is a tween');
+
+} else {
+	console.log('user is a child');
+
+} if (firstName ==='General' && lastName !=='Assembly') {
+	console.log('hello general, how are the troops?');
+}
+
+}
+
+// when the pagr has loaded
+$(function() {
+
+	// when th user clicks an h3 element. Quote marks calls up from css
+	$('h3').on('click', function() {
+
+		// hide the text element. Toggling allows you to hide and reopen h1's etc. 
+		$(this).next().slideToggle(900);
+
+
+
+	});
+
+});
